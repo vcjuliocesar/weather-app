a7b337b4eac2873f9e76aff093c694723617ddae
julio cesar <vcjuliocesar@outlook.com>
2025-10-28 20:47:35 -0600

    feat(config): add basic project configuration setup

--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.views.generic.base import TemplateView
 # Create your views here.
+class HomePageView(TemplateView):
+    template_name = "main/home.html"
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #context[""] = 
+        return context
+    
