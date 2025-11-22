4ba96f72dfd3df9f3c80f57cb30a488dd2c20fdc
julio cesar <vcjuliocesar@outlook.com>
2025-11-21 18:57:33 -0600

    feat(views): create base dashboard views

--- main/views.py
+++ main/views.py
@@ -1,11 +1,48 @@
+import asyncio
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.http import JsonResponse, Http404
+from django.views.generic.base import FormView,TemplateView
+from django.views.decorators.http import require_GET
+from django.utils.decorators import method_decorator
+from .forms import SearchForm
+from .models import City, Search
+from .services import fetch_weather
 # Create your views here.
+
+
 class HomePageView(TemplateView):
-    template_name = "main/home.html"
+    template_name = "main/search.html"
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        #context[""] = 
-        return context
+
+class SearchView(TemplateView):
+    template_name = "main/search.html"
+    form_class = SearchForm
     
+    
+    def form_valid(self, form):
+        city = form.cleaned_data.get("city")
+        country = form.cleaned_data.get("country")
+        
+        Search.objects.create(
+            query=f"{city},{country}",
+            ip=self.request.META.get("REMOTE_ADDR"),
+        )
+        
+        return self.render_to_response(self.get_context_data(city=city, country=country))
+    
+@method_decorator(require_GET, name='dispatch')
+class WeatherDataView(TemplateView):
+    
+    def get(self,request,*args,**kwargs):
+        city = request.GET.get("city")
+        country = request.GET.get("country")
+        
+        if not city or not country:
+            raise Http404("Missing city")
+        
+        data = asyncio.run(fetch_weather(city,country))
+        return JsonResponse({
+            "city": data.city,
+            "country": data.country,
+            "current": data.current,
+        })
